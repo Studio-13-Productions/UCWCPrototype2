429a87787e49625124803e73c75569da1fedf249
Zachary Brown <studio13productions@outlook.com>
2021-10-21 17:55:54 -0400

    changed variables for boundaries

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10;
+
+    //Indicates the player boundaries of the game
+    public float boundary= 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime* speed);
+        //These two if statements defines the boundaries of the player
+        if(transform.position.x < -11){
+            transform.position = new Vector3(boundary*-1, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > 11){
+
+            transform.position = new Vector3(boundary, transform.position.y, transform.position.z);
         
+
+        }
     }
 }
